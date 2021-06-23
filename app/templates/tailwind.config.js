313d1a6515b29fb9b53f3188d5df517fd4dfeb20
sahildatax <84317311+sahildatax@users.noreply.github.com>
2021-06-23 10:31:34 +0530

    Add files via upload

---
+++ app/templates/tailwind.config.js
@@ -0,0 +1,10 @@
+ // tailwind.config.js
+ @import url('https://fonts.googleapis.com/css?family=Macondo');
+  module.exports = {
+    theme: {
+      fontFamily: {
+      'display': ['Macondo'],
+       'body': ['Macondo'],
+      }
+    }
+  }
